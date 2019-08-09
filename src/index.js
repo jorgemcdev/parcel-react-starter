7e58920f66855c87d2009bfc79b4692d77b34e6b
Jorge Carvalho <jorgecarvalho@MacBook-Pro-de-Jorge.local>
2019-08-09 16:45:30 +0100

    Add React demo

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './main.scss';
+
+const App = () => <h1>Hello Parcel React</h1>
+
+ReactDOM.render(<App />, document.querySelector('#root'));
