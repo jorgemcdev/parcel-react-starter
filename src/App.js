264e1fc49435d4b70246fec05b27646642bd2e08
Jorge Carvalho <jorgecarvalho@MBP-de-Jorge.lan>
2019-08-10 16:43:33 +0100

    Remove test class

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import parcelLogo from './images/parcel-logo.svg';
 const App = () => (
   <Fragment>
     <img src={parcelLogo} alt="parcel" width="200" />
-    <h1 class="black">Hello Parcel React</h1>
+    <h1>Hello Parcel React</h1>
   </Fragment>
 );
 
